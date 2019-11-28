ea2e925e010fa7869ef285d67a370d1499ff9680
Cameron Elwood <celwood93@gmail.com>
2019-11-27 20:03:39 -0800

    Updates

---
+++ precache-manifest.de62cabc96db5cdff4bc34dc3c861a04.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a1fc7ff1756618d8340be63ade6554",
+    "url": "/edci337a4/index.html"
+  },
+  {
+    "revision": "8a22e0ccf35b2a398026",
+    "url": "/edci337a4/static/css/main.231dbd54.chunk.css"
+  },
+  {
+    "revision": "183f3db29571d00e073a",
+    "url": "/edci337a4/static/js/2.f9a9c346.chunk.js"
+  },
+  {
+    "revision": "8a22e0ccf35b2a398026",
+    "url": "/edci337a4/static/js/main.c7ba489c.chunk.js"
+  },
+  {
+    "revision": "6b95c3e7c3c96f8c4c58",
+    "url": "/edci337a4/static/js/runtime-main.9d55790e.js"
+  },
+  {
+    "revision": "304fce081fcf7da7f6a4ddabe3126473",
+    "url": "/edci337a4/static/media/1024px-Tesla_Model_3_parked,_front_driver_side.304fce08.jpg"
+  },
+  {
+    "revision": "59259f83fbc778368f998e034cb6994d",
+    "url": "/edci337a4/static/media/1769_Cugnot_SteamTractor3.59259f83.jpg"
+  },
+  {
+    "revision": "e010d834e5ab913286c5deb271cbc5de",
+    "url": "/edci337a4/static/media/220px-1888_Flocken_Elektrowagen.e010d834.jpg"
+  },
+  {
+    "revision": "3bf535b5fb411f57f7726f86ab3c9d71",
+    "url": "/edci337a4/static/media/220px-Benz-1.3bf535b5.jpg"
+  },
+  {
+    "revision": "703690d8e8bb05b6f316ccaf78eab637",
+    "url": "/edci337a4/static/media/220px-Bugatti_Chiron_Sport_Genf_2018.703690d8.jpg"
+  },
+  {
+    "revision": "93298040d8922ef76a2492bbce274173",
+    "url": "/edci337a4/static/media/280px-1925_Ford_Model_T_touring.93298040.jpg"
+  },
+  {
+    "revision": "d7a5885161743d89eab77950f5d4d4fb",
+    "url": "/edci337a4/static/media/280px-Morris_Mini-Minor_1959_(621_AOK).d7a58851.jpg"
+  },
+  {
+    "revision": "8d399deebb049d9a0e739ace378fdd65",
+    "url": "/edci337a4/static/media/Beetle.8d399dee.jpg"
+  },
+  {
+    "revision": "8e060056aa07f590d59b91aca1a7f4a1",
+    "url": "/edci337a4/static/media/Bugatti_Typ_35C_Grand_Prix_Racer_1926.8e060056.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c7c1d0569474bb79feac1c05a6c4b16.js"
+  "/edci337a4/precache-manifest.de62cabc96db5cdff4bc34dc3c861a04.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/edci337a4/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
